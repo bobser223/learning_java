a355e1552ee455a1c8bc7155ef25b8072069f6f1
bobser_mac <bobser223@users.noreply.github.com>
2025-10-14 17:03:41 +0300

    HW 5 2 bug fixes and test data

--- src/hw/hw005/B05_02.java
+++ src/hw/hw005/B05_02.java
@@ -3,8 +3,22 @@ package hw.hw005;
 public class B05_02 {
 
     public static void main(String[] args) {
+        System.out.println("a-tests:");
+        System.out.println(a("3abc"));
+        System.out.println(a("0abc"));
+        System.out.println(a("2ab1"));
 
+        System.out.println("b-tests:");
+        System.out.println(b("5abcd"));
+        System.out.println(b("a2b3c"));
+        System.out.println(b("abc"));
+
+        System.out.println("c-tests:");
+        System.out.println(c("a2b3c"));
+        System.out.println(c("11111"));
+        System.out.println(c("999"));
     }
+
 
     public static boolean a(String s){
         if (s == null || s.isEmpty()) return false;
@@ -12,17 +26,18 @@ public class B05_02 {
         char[] chars = s.toCharArray();
         int cnt;
 
-        if (Character.isDigit(chars[0]) || chars[0] == '0'){
+        if (Character.isDigit(chars[0]) && chars[0] != '0'){
             cnt = Character.getNumericValue(chars[0]);
         } else
             return false;
 
-        for (char ch: chars){
-            if (Character.isDigit(ch))
+
+        for (int i = 1; i < chars.length; i++){
+            if (Character.isDigit(chars[i]))
                 return false;
         }
 
-        return s.length() == cnt;
+        return s.length() == (cnt+1);
     }
 
 
@@ -36,7 +51,9 @@ public class B05_02 {
                     cnt = Character.getNumericValue(ch);
                 else
                     return false;
-            }
+            } else if (!Character.isLetter(ch)) {
+            return false;
+        }
         }
         return s.length() == cnt;
     }
